a05980e19b623432ea2b45e6cb0a23a3dbe635e9
MrGru <duan.hitex@gmail.com>
2024-04-24 03:43:05 +0700

    animated sprites

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ fn main() {
             ..Default::default()
         }))
         .add_systems(Startup, (spawn_camera, draw_the_player))
+        .add_systems(Update, animated_sprite)
         .run();
 }
 
@@ -22,9 +23,51 @@ fn spawn_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 }
 
-fn draw_the_player(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(SpriteBundle {
-        texture: asset_server.load("gfx/characters/player.png"),
-        ..SpriteBundle::default()
-    });
+#[derive(Component)]
+struct AnimationIndices {
+    first: usize,
+    last: usize,
+}
+
+#[derive(Component, Deref, DerefMut)]
+struct AnimationTimer(Timer);
+
+fn animated_sprite(
+    time: Res<Time>,
+    mut query: Query<(&AnimationIndices, &mut AnimationTimer, &mut TextureAtlas)>,
+) {
+    for (indices, mut timer, mut atlas) in &mut query {
+        timer.tick(time.delta());
+        if timer.just_finished() {
+            atlas.index = if atlas.index == indices.last {
+                indices.first
+            } else {
+                atlas.index + 1
+            }
+        }
+    }
+}
+fn draw_the_player(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
+) {
+    let texture = asset_server.load("gfx/characters/player.png");
+    let layout = TextureAtlasLayout::from_grid(Vec2::new(128.0, 128.0), 4, 4, None, None);
+    let texture_atlas_layout = texture_atlas_layouts.add(layout);
+    let animation_indices = AnimationIndices { first: 0, last: 3 };
+
+    commands.spawn((
+        SpriteBundle {
+            texture,
+            transform: Transform::from_scale(Vec3::new(1.0, 1.0, 1.0)),
+            ..SpriteBundle::default()
+        },
+        TextureAtlas {
+            layout: texture_atlas_layout,
+            index: animation_indices.first,
+        },
+        animation_indices,
+        AnimationTimer(Timer::from_seconds(0.2, TimerMode::Repeating)),
+    ));
 }
