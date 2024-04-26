69e86bb3dbe1799e00556535710b8975e47138c3
MrGru <duan.hitex@gmail.com>
2024-04-26 22:26:17 +0700

    using bevy asset loader

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use bevy::prelude::*;
+use bevy_asset_loader::{
+    asset_collection::AssetCollection,
+    loading_state::{config::ConfigureLoadingState, LoadingState, LoadingStateAppExt},
+};
 
 pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
 pub const RESOLUTION: f32 = 16.0 / 9.0;
 
 fn main() {
     App::new()
+        .init_state::<MyStates>()
         .insert_resource(ClearColor(CLEAR))
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
@@ -14,8 +19,14 @@ fn main() {
             }),
             ..Default::default()
         }))
-        .add_systems(Startup, (spawn_camera, draw_the_player))
-        .add_systems(Update, animated_sprite)
+        .add_loading_state(
+            LoadingState::new(MyStates::AssetLoading)
+                .continue_to_state(MyStates::Next)
+                .load_collection::<MyAssets>(),
+        )
+        // .add_systems(Startup, (spawn_camera, draw_the_player))
+        .add_systems(OnEnter(MyStates::Next), (spawn_camera, draw_the_player))
+        .add_systems(Update, animated_sprite.run_if(in_state(MyStates::Next)))
         .run();
 }
 
@@ -47,27 +58,42 @@ fn animated_sprite(
         }
     }
 }
-fn draw_the_player(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
-) {
-    let texture = asset_server.load("gfx/characters/player.png");
-    let layout = TextureAtlasLayout::from_grid(Vec2::new(128.0, 128.0), 4, 4, None, None);
-    let texture_atlas_layout = texture_atlas_layouts.add(layout);
+
+#[derive(AssetCollection, Resource)]
+struct MyAssets {
+    #[asset(texture_atlas_layout(
+        tile_size_x = 128.0,
+        tile_size_y = 128.0,
+        columns = 4,
+        rows = 4
+    ))]
+    player_layout: Handle<TextureAtlasLayout>,
+    #[asset(image(sampler = nearest))]
+    #[asset(path = "gfx/characters/player.png")]
+    player_texture: Handle<Image>,
+}
+
+fn draw_the_player(mut commands: Commands, my_assets: Res<MyAssets>) {
     let animation_indices = AnimationIndices { first: 0, last: 3 };
 
     commands.spawn((
         SpriteBundle {
-            texture,
+            texture: my_assets.player_texture.clone(),
             transform: Transform::from_scale(Vec3::new(1.0, 1.0, 1.0)),
             ..SpriteBundle::default()
         },
         TextureAtlas {
-            layout: texture_atlas_layout,
+            layout: my_assets.player_layout.clone(),
             index: animation_indices.first,
         },
         animation_indices,
         AnimationTimer(Timer::from_seconds(0.2, TimerMode::Repeating)),
     ));
+}
+
+#[derive(Clone, Eq, PartialEq, Debug, Hash, Default, States)]
+enum MyStates {
+    #[default]
+    AssetLoading,
+    Next,
 }
